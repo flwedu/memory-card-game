f87ec34d6633f032dd6c5b8f920ee66a96ab380d
Eduardo Aquino <edutraquino@gmail.com>
2022-03-31 22:08:21 -0300

    chore: add copy-webpack-plugin lib

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -29,6 +30,14 @@ module.exports = {
       template: "src/index.html",
       filename: "index.html",
       hash: true,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets",
+          to: "assets",
+        },
+      ],
     }),
   ],
   devServer: {
