da70f148ca77f69b486bc9e7bd557446e724100e
Eduardo Aquino <edutraquino@gmail.com>
2022-04-02 12:05:42 -0300

    test: add app test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,7 +1,30 @@
+import App from "./app"
+
 describe("App class tests", () => {
 
-    test.skip("Should ", () => {
+    beforeEach(() => {
+        document.body.innerHTML = ``;
+        setup();
+    })
 
-        return true;
+    test("checkMatch method should be called only if are two cards in pairCards list", () => {
+
+        const app = new App([1, 1]);
+        const spy = jest.spyOn(app, "checkMatch");
+        const cards = app.cardList.cards;
+
+        app.addCardToPair(cards[0]);
+
+        expect.assertions(2);
+        expect(spy).not.toHaveBeenCalled();
+
+        app.addCardToPair(cards[1]);
+        expect(spy).toHaveBeenCalled();
     })
 })
+
+function setup() {
+    const cardList = document.createElement("div");
+    cardList.id = "cardList";
+    document.body.appendChild(cardList);
+}
