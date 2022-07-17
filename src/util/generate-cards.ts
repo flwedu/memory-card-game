4188b4ca19dbeda52e1d4b1931cec1800128e688
Eduardo Aquino <edutraquino@gmail.com>
2022-07-17 19:03:14 -0300

    feat: add method to generate cards from array

--- src/util/generate-cards.ts
+++ src/util/generate-cards.ts
@@ -1,17 +1,23 @@
 import Card from "../components/Card";
 import { shuffleArray } from "./shuffle-array";
 
-export function generateCards(cardNumber: number) {
+export function generateCardsArrayWithDoubleLength(length: number) {
   const numberArray: number[] = [];
 
-  for (let i = 0; i < cardNumber; i++) {
+  for (let i = 0; i < length; i++) {
     numberArray.push(i + 1);
     numberArray.push(i + 1);
   }
 
   const shuffledArray = shuffleArray(numberArray);
 
-  const cards = shuffledArray.map((value, index) => {
+  const cards = generateCardsArrayFromArray(shuffledArray);
+
+  return cards;
+}
+
+export function generateCardsArrayFromArray(array: number[]) {
+  const cards = array.map((value, index) => {
     return new Card(value, index);
   });
 
