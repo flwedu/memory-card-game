b4b96a7cfaea900051a44846942dbe45a5abae18
Eduardo Aquino <edutraquino@gmail.com>
2022-07-17 12:07:09 -0300

    test(CardList): add tests to new class methods

--- src/components/CardList.spec.ts
+++ src/components/CardList.spec.ts
@@ -25,4 +25,61 @@ describe("CardList class tests", () => {
     expect(spy).toHaveBeenCalled();
     expect(firstCard.flipped).toBe(true);
   });
+
+  test("getSelectedCards() should return the selected cards", () => {
+    const div = document.createElement("div");
+    const cards = numberArr.map((value, index) => new Card(value, index));
+    const cardList = new CardList(div, cards);
+    const firstCard = cards[0];
+    const secondCard = cards[1];
+
+    const firstCardEl = firstCard.getEl();
+    const secondCardEl = secondCard.getEl();
+
+    expect(cardList.getSelectedCards()).toEqual([]);
+
+    firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+    expect(cardList.getSelectedCards()).toEqual([firstCard, secondCard]);
+  });
+
+  test("unFlipSelectedCards() should unFlip the selected cards", () => {
+    const div = document.createElement("div");
+    const cards = numberArr.map((value, index) => new Card(value, index));
+    const cardList = new CardList(div, cards);
+    const firstCard = cards[0];
+    const secondCard = cards[1];
+
+    const firstCardEl = firstCard.getEl();
+    const secondCardEl = secondCard.getEl();
+
+    firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+    cardList.unFlipSelectedCards();
+
+    expect(firstCard.flipped).toBe(false);
+    expect(secondCard.flipped).toBe(false);
+    expect(cardList.getSelectedCards()).toEqual([]);
+  });
+
+  test("setMatchedToSelectedCards() should set the matched cards", () => {
+    const div = document.createElement("div");
+    const cards = numberArr.map((value, index) => new Card(value, index));
+    const cardList = new CardList(div, cards);
+    const firstCard = cards[0];
+    const secondCard = cards[1];
+
+    const firstCardEl = firstCard.getEl();
+    const secondCardEl = secondCard.getEl();
+
+    firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
+    cardList.setMatchedToSelectedCards();
+
+    expect(firstCard.isMatched()).toBe(true);
+    expect(secondCard.isMatched()).toBe(true);
+  });
 });
