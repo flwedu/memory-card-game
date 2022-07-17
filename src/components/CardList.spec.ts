906498e0a716d348e306486c1a4940326fd1f775
Eduardo Aquino <edutraquino@gmail.com>
2022-07-17 19:04:14 -0300

    tests: to checkSelectedCardsMatch() method

--- src/components/CardList.spec.ts
+++ src/components/CardList.spec.ts
@@ -1,4 +1,4 @@
-import Card from "./Card";
+import { generateCardsArrayFromArray } from "../util/generate-cards";
 import CardList from "./CardList";
 
 describe("CardList class tests", () => {
@@ -44,42 +44,43 @@ describe("CardList class tests", () => {
     expect(cardList.getSelectedCards()).toEqual([firstCard, secondCard]);
   });
 
-  test("unFlipSelectedCards() should unFlip the selected cards", () => {
-    const div = document.createElement("div");
-    const cards = numberArr.map((value, index) => new Card(value, index));
-    const cardList = new CardList(div, cards);
-    const firstCard = cards[0];
-    const secondCard = cards[1];
+  describe("checkSelectedCardsMatch()", () => {
+    test("should return true if the selected cards match, and getFlippedCards() should return 2 cards", () => {
+      const numberArr = [1, 1];
+      const div = document.createElement("div");
+      const cards = generateCardsArrayFromArray(numberArr);
+      const cardList = new CardList(div, cards);
+      const firstCard = cards[0];
+      const secondCard = cards[1];
 
-    const firstCardEl = firstCard.getEl();
-    const secondCardEl = secondCard.getEl();
+      const firstCardEl = firstCard.getEl();
+      const secondCardEl = secondCard.getEl();
 
-    firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-    secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
 
-    cardList.unFlipSelectedCards();
+      expect(cardList.checkSelectedCardsMatch()).toBe(true);
+      expect(cardList.getFlippedCards().length).toBe(2);
+      expect(cardList.getUnmatchedCards().length).toBe(0);
+    });
 
-    expect(firstCard.flipped).toBe(false);
-    expect(secondCard.flipped).toBe(false);
-    expect(cardList.getSelectedCards()).toEqual([]);
-  });
+    test("should return false if the selected cards don't match, and getFlippedCards() should return 0 cards", () => {
+      const numberArr = [1, 2];
+      const div = document.createElement("div");
+      const cards = generateCardsArrayFromArray(numberArr);
+      const cardList = new CardList(div, cards);
+      const firstCard = cards[0];
+      const secondCard = cards[1];
 
-  test("setMatchedToSelectedCards() should set the matched cards", () => {
-    const div = document.createElement("div");
-    const cards = numberArr.map((value, index) => new Card(value, index));
-    const cardList = new CardList(div, cards);
-    const firstCard = cards[0];
-    const secondCard = cards[1];
+      const firstCardEl = firstCard.getEl();
+      const secondCardEl = secondCard.getEl();
 
-    const firstCardEl = firstCard.getEl();
-    const secondCardEl = secondCard.getEl();
+      firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
 
-    firstCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-    secondCardEl.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-
-    cardList.setMatchedToSelectedCards();
-
-    expect(firstCard.isMatched()).toBe(true);
-    expect(secondCard.isMatched()).toBe(true);
+      expect(cardList.checkSelectedCardsMatch()).toBe(false);
+      expect(cardList.getFlippedCards().length).toBe(0);
+      expect(cardList.getUnmatchedCards().length).toBe(2);
+    });
   });
 });
