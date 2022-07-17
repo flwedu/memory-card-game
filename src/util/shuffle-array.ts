fe6c422d7044d5a1edcf79a08ff98876d0c48a88
Eduardo Aquino <edutraquino@gmail.com>
2022-07-17 10:23:21 -0300

    refact: change return type to number[]

--- src/util/shuffle-array.ts
+++ src/util/shuffle-array.ts
@@ -1,13 +1,16 @@
-export function shuffle(array: any[]) {
-    let lastIndex = array.length, randomIndex;
+export function shuffleArray(array: number[]) {
+  let lastIndex = array.length,
+    randomIndex;
 
-    while (lastIndex != 0) {
+  while (lastIndex != 0) {
+    randomIndex = Math.floor(Math.random() * lastIndex);
+    lastIndex--;
 
-        randomIndex = Math.floor(Math.random() * lastIndex);
-        lastIndex--;
+    [array[lastIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[lastIndex],
+    ];
+  }
 
-        [array[lastIndex], array[randomIndex]] = [array[randomIndex], array[lastIndex]];
-    }
-
-    return array;
+  return array;
 }
