b146cb00aeabd6a73c371da24c471f3200d906ab
Eduardo Aquino <edutraquino@gmail.com>
2022-07-17 11:04:41 -0300

    feat: handling click on img and add size className

--- src/components/CardList.ts
+++ src/components/CardList.ts
@@ -9,7 +9,9 @@ export default class CardList implements Component {
     this.el = el;
     this.cards = cards;
 
-    this.el.className = "card-list";
+    const gameSize = (this.cards.length / 2) as number;
+
+    this.el.classList.add(`size-${gameSize}`);
     this.el.addEventListener("click", this.handleClick.bind(this));
 
     const cardElements = this.cards.map((card) => card.getEl());
@@ -17,9 +19,14 @@ export default class CardList implements Component {
   }
 
   handleClick(event: MouseEvent): void {
-    const target = event.target as HTMLElement;
-    if (target.classList.contains("card")) {
-      const cardIndex = Number(target.getAttribute("data-index"));
+    let el = event.target as HTMLElement;
+
+    // Check click on a img
+    if (el.tagName === "IMG") {
+      el = el.closest(".card");
+    }
+    if (el.classList.contains("card")) {
+      const cardIndex = Number(el.getAttribute("data-index"));
       this.cards[cardIndex].flip();
     }
   }
